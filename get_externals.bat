7243129d8de56bd8e1ba8e8db56caabd0d9c3164
AraHaan <seandhunt_7@yahoo.com>
2025-12-30 03:41:42 -0500

    Added get_externals.bat and upgrade miniz to version 3.1.0.
    Signed-off-by: AraHaan <seandhunt_7@yahoo.com>

---
+++ get_externals.bat
@@ -0,0 +1,7 @@
+@echo off
+mkdir externals
+cd externals
+git clone https://github.com/AraHaan/py2exe --branch cp313
+curl -L -o "%~dp0externals\miniz-3.1.0.zip" ^ https://github.com/richgel999/miniz/releases/download/3.1.0/miniz-3.1.0.zip
+mkdir "miniz-3.1.0"
+tar -xf "%~dp0externals\miniz-3.1.0.zip" -C "%~dp0externals\miniz-3.1.0"
