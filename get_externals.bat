4eb409bb2dca2d6dc8316373f21f1b5a29202bf7
AraHaan <seandhunt_7@yahoo.com>
2025-12-30 03:57:33 -0500

    Forgot to add the audioop external as well.
    Signed-off-by: AraHaan <seandhunt_7@yahoo.com>

--- get_externals.bat
+++ get_externals.bat
@@ -1,7 +1,8 @@
 @echo off
 mkdir externals
 cd externals
-git clone https://github.com/AraHaan/py2exe --branch cp313
+git clone https://github.com/AraHaan/py2exe.git --branch cp313
+git clone https://github.com/AraHaan/audioop.git
 curl -L -o "%~dp0externals\miniz-3.1.0.zip" ^ https://github.com/richgel999/miniz/releases/download/3.1.0/miniz-3.1.0.zip
 mkdir "miniz-3.1.0"
 tar -xf "%~dp0externals\miniz-3.1.0.zip" -C "%~dp0externals\miniz-3.1.0"
